2b45e65df4da998bcc2980c45be4a8b750ffc4e0
artwmana <artwmana@gmail.com>
2025-11-24 20:58:25 +0300

    Update adding test column

--- db/init/02_insert_students.sql
+++ db/init/02_insert_students.sql
@@ -1,5 +1,4 @@
 INSERT INTO students (student_id, name, group_name)
 VALUES
-    ('S001', 'Artem Morozov', '51'),
-    ('S002', 'Ivan', '52')
+    ('55', 'Artem', '578105')
 ON CONFLICT (student_id) DO NOTHING;

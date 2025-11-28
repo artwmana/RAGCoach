a34c3878be2b3bea767776f0af86def7e825487d
artwmana <artwmana@gmail.com>
2025-11-28 18:15:02 +0300

    Remove unused scripts

--- 02_insert_students.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO students (student_id, name, group_name, email)
-VALUES
-    ('S001', 'Artem Morozov', 'CS-21', 'artem@example.com'),
-    ('S002', 'Ivan Petrov', 'CS-22', 'ivan@example.com')
-ON CONFLICT (student_id) DO NOTHING;
--- db/init/02_insert_students.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO students (student_id, name, group_name)
-VALUES
-    ('55', 'Artem', '578105')
-ON CONFLICT (student_id) DO NOTHING;
